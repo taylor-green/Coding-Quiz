36126dc48790219f2be51a1963b4eb2171fafd33
taylor-green <taylor.green2804@gmail.com>
2023-02-17 13:39:00 -0500

    created fucntion for questions and grabbing new ones, began event listener for selected choices

--- Assets/quiz.js
+++ Assets/quiz.js
@@ -5,7 +5,6 @@ var scoreText = document.querySelector('#score');
 var currentQuestion = {}
 var acceptedAnswer = true
 var score = 0
-var questionCounter = 0
 var allQuestions =[]
 
 var questions =[
@@ -51,60 +50,71 @@ var questions =[
     }
 ]
 
+const SCORE_POINTS = 100
 
-startButton.addEventListener('click',
-    function () {
 
-        var timeLeft = 30;
+startQuiz = () => {
+    score = 0
+    allQuestions = [...questions]
+    getNewQuestion()
 
-        var timeInterval = setInterval(
-            function () {
-                if (timeLeft > 0) {
-                    timerCount.textContent = timeLeft;
-
-                    timeLeft--;
-                    console.log("hello");
-                } else {
-                    timerCount.textContent = timeLeft;
-                    clearInterval(timeInterval);
-                }
-
-            }, 1000)
-    });
-
-var question = {
-    title: "Which of the following is considered a string?",
-    choices: ["'Mike'", "13", "True", "0.4376"],
-    correctAnswer: 0
-};
-
-function showQuestion(q) {
-    // select DOM element to modify
-    var titleDiv = document.getElementById('title');
-
-    // modify element
-    titleDiv.textContent = q.title;
-
-    // select by query
-    var choice = document.querySelectorAll(".choices");
-    console.log(choice);
-
-    choice.forEach(function (element, index) {
-        element.textContent = q.choices[index];
-
-        element.addEventListener('click', function(){
-            if(q.correctAnswer == index) {
-            console.log('Correct!');
-        } else {
-            console.log('Wrong!');
-        }
-    });
-  });
 }
-showQuestion(question);
 
-var btn = document.getElementById('btn');
-btn.addEventListener('click', function() {
+getNewQuestion = () => {
+    if(allQuestions.length === 0) {
+        localStorage.setItem('mostRecentScore', score)
 
-});
+        return window.location.assign('/end.html')
+    }
+
+    var questionsIndex = Math.floor(Math.random() * allQuestions.length)
+    currentQuestion = allQuestions[questionsIndex]
+    question.innerText = currentQuestion.question
+
+    choices.forEach(choice => {
+        var number = choice.dataset['number']
+        choice.innerText = currentQuestion['choice' + number]
+    })
+
+    allQuestions.splice(questionsIndex, 1)
+
+    acceptedAnswer = true
+}
+
+choice.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptedAnswer) return
+
+        acceptedAnswer = false
+        var selectedChoice = e.target
+        var selectedAnswer = selectedChoice.dataset['number']
+
+        if (selectedAnswer === true){
+            incrementScore(SCORE_POINTS)
+        }
+    })
+
+})  
+
+
+
+    // // function () {
+
+    //     var timeLeft = 30;
+
+    //     var timeInterval = setInterval(
+    //         function () {
+    //             if (timeLeft > 0) {
+    //                 timerCount.textContent = timeLeft;
+
+    //                 timeLeft--;
+    //                 console.log("hello");
+    //             } else {
+    //                 timerCount.textContent = timeLeft;
+    //                 clearInterval(timeInterval);
+    //             }
+
+    //         }, 1000)
+    // });
+
 
